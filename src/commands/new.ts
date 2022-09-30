718d43b255fe2300cdc466f8fad83058728a8798
أحمد ماهر صبيح <32990360+ahmedmaher30190@users.noreply.github.com>
2022-09-30 22:35:12 +0200

    first commit

---
+++ src/commands/new.ts
@@ -0,0 +1,44 @@
+import { Command, flags } from '@oclif/command';
+import { execSync } from 'child_process';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+export default class New extends Command {
+  static description = 'Scaffold a new Laravel + Next.js application';
+
+  static examples = ['$ create-nextstream-app new app'];
+
+  static flags = {
+    debug: flags.boolean({ char: 'v', description: 'debug output' }),
+  };
+
+  static args = [{ name: 'name' }];
+
+  async run() {
+    const { args, flags } = this.parse(New);
+
+    this.log('Creating frontend project...');
+    const rootFolder = path.join(process.cwd(), args.name);
+    const frontendFolder = path.join(rootFolder, 'frontend');
+    const backendFolder = path.join(rootFolder, 'backend');
+    fs.mkdirSync(rootFolder);
+    execSync(
+      'git clone https://github.com/ozziexsh/nextstream-ts.git frontend',
+      { cwd: rootFolder },
+    );
+    execSync('rm -rf frontend/.git', { cwd: rootFolder });
+
+    this.log('Installing npm dependencies');
+    execSync('npm i', { cwd: frontendFolder });
+
+    this.log('Creating environment file');
+    execSync('cp .env.local.example .env.local', { cwd: frontendFolder });
+
+    this.log('Creating Laravel project...');
+    execSync(`git clone https://github.com/laravel/laravel.git backend`, { cwd: rootFolder });
+
+    this.log('Installing Nextstream...');
+    execSync(`composer require ozzie/nextstream`, { cwd: backendFolder });
+    execSync(`php artisan nextstream:install`, { cwd: backendFolder });
+  }
+}
